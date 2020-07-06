2998d8462e1cbbae945c83e69411e449a5671215
Timofey Arkhangelskiy <timarkh@gmail.com>
2020-07-06 11:25:14 +0200

    Now the original Swedish sentence is replaced with an analyzed one in the XML file.

--- xml2txt.py
+++ xml2txt.py
@@ -16,7 +16,7 @@ def xml2txt(fnameIn, fnameOut):
     text = ''
     sentences = xmlTree.xpath('/html/body/para/se[@lang="sv"] | /html/body/p/para/se[@lang="sv"]')
     for sentence in sentences:
-        text += sentence.xpath('string()') + '\r\n'
+        text += sentence.xpath('string()').replace('\n', '<br>') + '\r\n'
     fOut = open(fnameOut, 'w', encoding='utf-8')
     fOut.write(text)
     fOut.close()
@@ -120,47 +120,70 @@ def parsed2xml_old(fnameInXml, fnameInParsed, fnameOut):
         print('Possible tokenization problem.')
 
 
+def build_ana(token, lemma, pos1, gramm, curAddr, prevAddr):
+    if re.search('^\n+$', lemma, flags=re.DOTALL) is not None:
+        return ''
+    ana = ' ' * (curAddr - prevAddr)
+    if pos1 not in ['PAD', 'MAD', 'MID', 'PID']:
+        ana += '<w><ana lex="' + escape(lemma) + '" gr="' + \
+              pos1
+        if len(gramm) > 0 and gramm != '_':
+            ana += ',' + gramm.lower().replace('|', ',')
+        ana += '" />'
+    ana += token
+    if pos1 not in ['PAD', 'MAD', 'MID', 'PID']:
+        ana += '</w>'
+    return ana
+
+
 def parsed2xml(fnameInXml, fnameInParsed, fnameOut):
+    """
+    Read a file analyzed by stagger and the aligned Swedish/Russian XML.
+    Generate an XML where the Swedish part is analyzed.
+    It is assumed that sentence separation in the parallel XML was identical
+    to the line separation in the input file given to stagger. The original
+    line separation is deduced from the \r character in the stagger-analyzed
+    file, so Windows newlines must be used in the stagger input file.
+    """
     print(fnameInXml)
-    fXml = open(fnameInXml, 'r', encoding='utf-8-sig')
-    fParsed = open(fnameInParsed, 'r', encoding='utf-8')
-    parsedWords = {}
-    addEscape = 0
-    for line in fParsed:
-        if len(line) < 3:
-            continue
+    fParsed = open(fnameInParsed, 'r', encoding='utf-8', newline='')
+    text = fParsed.read()
+    fParsed.close()
+    lines = re.findall('(?:[^\t]*\t){12}[^\t\n]*\n*', text, flags=re.DOTALL)
+    sentences = ['']
+    iSent = 0
+    prevAddr = 0
+    for line in lines:
         try:
             numToken, token, lemma, pos1, pos2, gramm,\
-                _1, _2, _3, _4, _5, _6, address = line.strip().split('\t')
+                _1, _2, _3, _4, _5, _6, address = line.strip('\n').split('\t')
         except ValueError:
+            print('Error when splitting line: ' + line)
             continue
-        address = int(address.split(':')[-1]) + addEscape
-        addEscape += len(escape(token)) - len(token)
-        ana = token
-        if token in ['…', '“', '„', '~', '’', '»', '«', '—', '•', '√', '–', '']:
+        if token == '\r':
+            iSent += 1
+            sentences.append('')
             continue
-        if pos1 not in ['PAD', 'MAD', 'MID', 'PID']:
-            ana = '<w><ana lex="' + escape(lemma) + '" gr="' +\
-                  pos1
-            if len(gramm) > 0 and gramm != '_':
-                ana += ',' + gramm.lower().replace('|', ',')
-            ana += '" />'
-            if address in parsedWords:
-                print('Duplicate token address: ' + str(address) + ', token: ' + token)
-            parsedWords[address] = (escape(token), ana)
-    fParsed.close()
-    fOut = open(fnameOut, 'w', encoding='utf-8')
+        curAddr = int(address.split(':')[-1])
+        sentences[iSent] += build_ana(token, lemma, pos1, gramm, curAddr, prevAddr)
+        prevAddr = curAddr + len(token)
+    sentences = [s.strip() for s in sentences]
+    print(sentences)
+
+    #fOut = open(fnameOut, 'w', encoding='utf-8')
     paraId = -1
-    iSe = -1
-    startChar = 0
+    iSe = 0
     lang = 'ru'
+    fXml = open(fnameInXml, 'r', encoding='utf-8-sig')
     xmlText = fXml.read()
-    xmlText = re.sub('(\n+)(?=</se>)', lambda m: len(m.group(1)) * 2 * ' ', xmlText, flags=re.DOTALL).split('\n')
+    xmlText = re.sub('(\n+)(?=</se>)',
+                     lambda m: len(m.group(1)) * 2 * ' ',
+                     xmlText, flags=re.DOTALL).split('\n')
     fXml.close()
     for line in xmlText:
         line += '\n'
         if re.search('^ *</?(body|head|html)> *\n', line) is not None:
-            fOut.write(line)
+            #fOut.write(line)
             continue
         if line.strip().startswith('<weight'):
             continue
@@ -173,49 +196,39 @@ def parsed2xml(fnameInXml, fnameInParsed, fnameOut):
             mPara = re.search('^([ \t]*<para id=")([^"]+)(.*)', line, flags=re.DOTALL)
             if mPara is not None:
                 paraId += 1
-                fOut.write(mPara.group(1) + str(paraId) + mPara.group(3))
-            else:
-                fOut.write(line)
+                #fOut.write(mPara.group(1) + str(paraId) + mPara.group(3))
+            #else:
+                #fOut.write(line)
             continue
         lineSrc = m.group(2)
-        lineOut = m.group(1)
-        # print m.group(2)
-        iChar = startChar
-        while iChar < startChar + len(lineSrc):
-            if iChar not in parsedWords:
-                lineOut += lineSrc[iChar - startChar]
-                iChar += 1
-                continue
-            token, ana = parsedWords[iChar]
-            lineOut += ana + lineSrc[iChar-startChar:iChar-startChar+len(token)] + '</w>'
-            iChar += len(token)
-        startChar += len(lineSrc) + 3
-        lineOut = lineOut.replace('," />', '" />')
-        lineOut += m.group(3)
-        fOut.write(lineOut)
-    fOut.close()
+        lineOut = m.group(1) + sentences[iSe] + m.group(3)
+        iSe += 1
+        print(paraId, lineSrc)
+        print(lineOut)
+        #fOut.write(lineOut)
+    #fOut.close()
 
 
 if __name__ == '__main__':
     nFiles2process = 0
-    for root, dirs, files in os.walk('./texts_2019.02/media/'):
+    for root, dirs, files in os.walk('./texts_2020/'):
         for fname in files:
             if not fname.endswith('.xml') or fname.endswith('analyzed.xml'):
                 continue
             fnameFull = os.path.join(root, fname)
-            # print(fnameFull)
-            # xml2txt(fnameFull,
-            #         os.path.join(root, fname[:-3] + 'txt'))
-            # if not os.path.exists(os.path.join(root, fname[:-4]) + '-out.txt'):
-            #     print('java -jar stagger/stagger.jar -modelfile swedish.bin -tag '
-            #           + os.path.abspath(os.path.join(root, fname[:-4])) + '.txt >'
-            #           + os.path.abspath(os.path.join(root, fname[:-4])) + '-out.txt')
-            #     nFiles2process += 1
-            nFiles2process += 1
-            parsed2xml(fnameFull,
-                       fnameFull[:-4] + '-out.txt',
-                       os.path.join('texts_2019.02/texts_2019.02_processed',
-                                    fname[:-4] + '-analyzed.xml'))
+            print(fnameFull)
+            xml2txt(fnameFull,
+                    os.path.join(root, fname[:-3] + 'txt'))
+            if not os.path.exists(os.path.join(root, fname[:-4]) + '-out.txt'):
+                print('java -jar stagger/stagger.jar -modelfile swedish.bin -tag '
+                      + os.path.abspath(os.path.join(root, fname[:-4])) + '.txt >'
+                      + os.path.abspath(os.path.join(root, fname[:-4])) + '-out.txt')
+                nFiles2process += 1
+            # nFiles2process += 1
+            # parsed2xml(fnameFull,
+            #            fnameFull[:-4] + '-out.txt',
+            #            os.path.join('texts_2019.02/texts_2019.02_processed',
+            #                         fname[:-4] + '-analyzed.xml'))
     print('Files to process:', nFiles2process)
     #xml2txt(u'text_multi/chajka.xml', u'text_multi/chajka.txt')
     #xml2txt(u'text_multi/sad.xml', u'text_multi/sad.txt')
