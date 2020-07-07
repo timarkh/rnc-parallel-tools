480880c8377c01b81922b6b0b2a261d1d5b78989
Timofey Arkhangelskiy <timarkh@gmail.com>
2020-07-07 10:21:17 +0200

    Removed an old function and simplified process_swedish.py.

--- process_swedish.py
+++ process_swedish.py
@@ -4,11 +4,9 @@ import json
 from lxml import etree
 from html import escape, unescape
 
-reWords = re.compile('((?:<[^<>\r\n]+>)*)'
-                     '([^\\w<>]+|N:r\\b|t\\.o\\.m\\.|o\\.s\\.v\\.|bl\\.a\\.|'
-                     'fr\\.o\\.m\\.|t\\.ex\\.|[\\w-]+)((?:[.?!:;() ]*</[^<>\r\n]+>)*)',
-                     flags=re.DOTALL)
-reWordsSimple = re.compile('(?:^|\\b)[^<>]+?(?:\\b|$)', flags=re.DOTALL)
+
+rxWord = re.compile('\\w', flags=re.DOTALL)
+rxNewline = re.compile('^\n+$', flags=re.DOTALL)
 
 
 def xml2txt(fnameIn, fnameOut):
@@ -22,116 +20,21 @@ def xml2txt(fnameIn, fnameOut):
     fOut.close()
 
 
-def parsed2xml_old(fnameInXml, fnameInParsed, fnameOut):
-    fXml = open(fnameInXml, 'r', encoding='utf-8-sig')
-    fParsed = open(fnameInParsed, 'r', encoding='utf-8')
-    parsedWords = []
-    for line in fParsed:
-        if len(line) < 3:
-            continue
-        try:
-            numToken, token, lemma, pos1, pos2, gramm,\
-                _1, _2, _3, _4, _5, _6, address = line.strip().split(u'\t')
-        except ValueError:
-            continue
-        address = int(address.split(':')[-1])
-        for token in token.split():
-            ana = token
-            if token in ['…', '“', '„', '~', '’', '»', '«', '—', '•', '√', '–']:
-                continue
-            if pos1 not in ['PAD', 'MAD', 'MID', 'PID']:
-                ana = '<w><ana lex="' + lemma + '" gr="' +\
-                      pos1
-                if len(gramm) > 0 and gramm != '_':
-                    ana += ',' + gramm.lower().replace('|', ',')
-                ana += '" />' + escape(token) + '</w>'
-                parsedWords.append((address, token, ana))
-    fParsed.close()
-    fOut = open(fnameOut, 'w', encoding='utf-8')
-    paraId = -1
-    iSe = -1
-    iWord = 0
-    lang = 'ru'
-    successiveNonAnalyzed = 0
-    bTokenizationProblem = False
-    for line in fXml:
-        if re.search('^ *</?(body|head|html)> *\n', line) is not None:
-            fOut.write(line)
-            continue
-        if 'lang="' in line:
-            m = re.search('lang="([^\r\n"]*)"', line)
-            lang = m.group(1)
-        m = re.search('^([ \t]*<se +lang="sv[^<>]*>|^)([^\r\n]*?)'
-                      '(</se>[ \t]*\n|\n)', line)
-        if m is None or lang != 'sv' or '<para' in m.group(2) or '</para' in m.group(2):
-            mPara = re.search('^([ \t]*<para id=")([^"]+)(.*)', line, flags=re.DOTALL)
-            if mPara is not None:
-                paraId += 1
-                fOut.write(mPara.group(1) + str(paraId) + mPara.group(3))
-            else:
-                fOut.write(line)
-            continue
-        lineOut = m.group(1)
-        # print m.group(2)
-        words = reWords.findall(m.group(2).replace('&quot;', '"').replace('&apos;', "'"))
-        for i in range(len(words) - 2, -1, -1):
-            # print words[i]
-            if ((words[i][1] == '-' or words[i+1][1].startswith('-') or
-                     words[i][1].endswith(':') or (words[i+1][1].startswith(':')
-                                                   and len(words[i+1][1]) > 1
-                                                   and not (re.search('[0-9]$', words[i][1]) is not None
-                                                            and re.search('^:[0-9 ]', words[i+1][1]) is not None)) or
-                     words[i][1].endswith("'") or words[i+1][1].startswith("'")) and
-                    words[i][2] != ' ') or words[i][2] == "'":
-                words[i] = list(words[i])
-                words[i][1] = words[i][1] + words[i+1][1]
-                words.pop(i+1)
-                words[i] = tuple(words[i])
-        for word in words:
-            wordStripped = word[1].strip()
-            if len(wordStripped) <= 0 or iWord >= len(parsedWords):
-                lineOut += word[0] + word[1] + word[2]
-                continue
-            # print wordStripped, iWord, parsedWords[iWord]
-            anaWritten = False
-            for i in range(iWord, min(iWord + 2, len(parsedWords))):
-                address, token, ana = parsedWords[i]
-                if token == wordStripped:
-                    lineOut += word[0] + ana + word[2]
-                    iWord = i + 1
-                    anaWritten = True
-                    break
-            if anaWritten:
-                successiveNonAnalyzed = 0
-                continue
-            if re.search('\\w', word[1]) is not None:
-                iWord += 1
-                lineOut += word[0] + '<w>' + escape(word[1]) + '</w>' + word[2]
-                successiveNonAnalyzed += 1
-                if successiveNonAnalyzed > 200:
-                    bTokenizationProblem = True
-            else:
-                lineOut += word[0] + escape(word[1]) + word[2]
-        lineOut = lineOut.replace('," />', '" />')
-        lineOut += m.group(3)
-        fOut.write(lineOut)
-    fOut.close()
-    if bTokenizationProblem:
-        print('Possible tokenization problem.')
-
-
 def build_ana(token, lemma, pos1, gramm, curAddr, prevAddr):
-    if re.search('^\n+$', lemma, flags=re.DOTALL) is not None:
+    if rxNewline.search(lemma) is not None:
         return ''
+    bWord = True
+    if pos1 in ['PAD', 'MAD', 'MID', 'PID'] or rxWord.search(token) is None:
+        bWord = False
     ana = ' ' * (curAddr - prevAddr)
-    if pos1 not in ['PAD', 'MAD', 'MID', 'PID']:
+    if bWord:
         ana += '<w><ana lex="' + escape(lemma) + '" gr="' + \
               pos1
         if len(gramm) > 0 and gramm != '_':
             ana += ',' + gramm.lower().replace('|', ',')
         ana += '" />'
     ana += token
-    if pos1 not in ['PAD', 'MAD', 'MID', 'PID']:
+    if bWord:
         ana += '</w>'
     return ana
 
@@ -223,15 +126,24 @@ if __name__ == '__main__':
                 continue
             fnameFull = os.path.join(root, fname)
             print(fnameFull)
-            # xml2txt(fnameFull,
-            #         os.path.join(root, fname[:-3] + 'txt'))
-            # if not os.path.exists(os.path.join(root, fname[:-3]) + 'txt.conll'):
-            #     fnamesIn += os.path.abspath(os.path.join(root, fname[:-4])) + '.txt '
-            #     nFiles2process += 1
+            xml2txt(fnameFull,
+                    os.path.join(root, fname[:-3] + 'txt'))
+            if not os.path.exists(os.path.join(root, fname[:-3]) + 'txt.conll'):
+                fnamesIn += os.path.abspath(os.path.join(root, fname[:-4])) + '.txt '
+                nFiles2process += 1
+            nFiles2process += 1
+    print('Files to process:', nFiles2process)
+    print('java -jar stagger/stagger.jar -modelfile swedish.bin -tag ' + fnamesIn)
+    # TODO: Call stagger directly
+    x = input('Press any key after tagging is complete...')
+    for root, dirs, files in os.walk('./texts_2020/'):
+        for fname in files:
+            if not fname.endswith('.xml') or fname.endswith('analyzed.xml'):
+                continue
+            fnameFull = os.path.join(root, fname)
+            print(fnameFull)
             nFiles2process += 1
             parsed2xml(fnameFull,
                        fnameFull[:-3] + 'txt.conll',
                        os.path.join('texts_2020/texts_2020_processed',
                                     fname[:-4] + '-analyzed.xml'))
-    print('Files to process:', nFiles2process)
-    # print('java -jar stagger/stagger.jar -modelfile swedish.bin -tag ' + fnamesIn)
